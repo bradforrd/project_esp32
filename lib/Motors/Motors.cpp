f3455ac98ec00c439366855365198f9d103b03f1
im-ppaull <timotejbarabas@gmail.com>
2026-01-28 21:45:25 +0100

    feat: motor main function

--- lib/Motors/Motors.cpp
+++ lib/Motors/Motors.cpp
@@ -12,19 +12,28 @@ constexpr int backwardMotors[] = {
     RearRightBackward, RearLeftBackward
 };
 
+MotorDirection currentState = NULL;
+
 // Funkcia, ktora vypne motory, ktore pridu ako argument do funkcie
-void resetMotors(int* motors, int count) {
+void toggleMotors(int* motors, int count, bool state = false) {
     for (int i = 0; i < count; i++)
-        digitalWrite(motors[i], LOW);
+        digitalWrite(motors[i], state ? HIGH : LOW);
 }
 
 // Hlavna funkcia pre pohyb robota, ktora ma 2 argumenty [smer, pocet otacok kolesa]
 void move(MotorDirection direction, float rotations) {
-    if (direction == Forward) {
-        // Vypne vsetky backwardMotors
-        int count = sizeof(backwardMotors) / sizeof(backwardMotors[0]);
-        resetMotors(backwardMotors, count);
+    // Ak sa smer, ktory pride do funkcie nerovna aktualnemu smeru, tak sa vypnu vsetky motory
+    if (currentState != direction) {
+        int backwardCount = sizeof(backwardMotors) / sizeof(backwardMotors[0]);
+        toggleMotors(backwardMotors, backwardCount);
 
-
+        int forwardCount = sizeof(forwardMotors) / sizeof(forwardMotors[0]);
+        toggleMotors(forwardMotors, forwardCount);
     }
+
+    // Motory, ktore sa maju pustit
+    int[] motors = direction == Forward ? forwardMotors : backwardMotors;
+    int count = sizeof(motors) / sizeof(motors[0]);
+
+    toggleMotors(motors, count, true);
 }
